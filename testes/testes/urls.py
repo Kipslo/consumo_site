4df9426ba780f8a35955ea4b7c966518da855572
Kipslo <gbrlrdrgsdslvl@gmail.com>
2025-01-03 19:56:16 -0300

    testes

--- testes/testes/urls.py
+++ testes/testes/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("", views.index, name="index"),
     path("aplicacao/", include("aplicacao.urls")),
     path("admin/", admin.site.urls),
 ]
